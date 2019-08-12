b93d01ddf7e0757a02c4cefe5b5bb0052779046d
Jolivar Jr <jolivarjunior@gmail.com>
2019-08-12 09:23:55 -0300

    single-post

--- wp-content/themes/tema-padrao/category.php
+++ wp-content/themes/tema-padrao/category.php
@@ -1,14 +1,35 @@
 <!-- IN: category.php -->
 
 <?php get_template_part('includes/header-page'); ?>
-<?php if (have_posts()) : ?>
-	<?php while (have_posts()) : ?>
-		<?php the_post(); ?>
-		<a href="<?= get_permalink() ?>">
-			<?= get_the_title() ?>
-		</a>
-		<br />
-	<?php endwhile; ?>
-	<?= paginate_links() ?>
-<?php endif; ?>
+
+<main class="container">
+	<div class="row">
+		<div class="col-sm-8">
+			<?php if (have_posts()) : ?>
+				<?php while (have_posts()) : ?>
+					<?php the_post(); ?>
+					<a class="box-list-blog" href="<?= get_permalink() ?>">
+						<?= get_the_post_thumbnail($post->ID, 'loop_blog_thumbnail') ?>
+						<span class="description-post-list">
+							<span class="title"><?= get_the_title() ?></span>
+							<span class="description"><?= get_the_excerpt() ?></span>
+						</span>
+					</a>
+					<br />
+				<?php endwhile; ?>
+				<div class="pagination mb-4">
+					<?= paginate_links() ?>
+				</div>
+			<?php endif; ?>
+		</div>
+		<div class="col-sm-4">
+			<aside class="sidebar">
+				<?php if (is_active_sidebar('category-sidebar')) : ?>
+					<?php dynamic_sidebar('category-sidebar') ?>
+				<?php endif; ?>
+			</aside>
+		</div>
+	</div>
+</main>
+
 <?php get_template_part('includes/footer-page'); ?>
--- wp-content/themes/tema-padrao/single.php
+++ wp-content/themes/tema-padrao/single.php
@@ -1,17 +1,57 @@
 <!-- IN: single.php -->
 
 <?php get_template_part('includes/header-page'); ?>
-<main class="container content-page">
-	<?php if (have_posts()) : ?>
-		<?php while (have_posts()) : ?>
+
+<main id="single-blog" class="container">
+	<div class="row">
+		<div class="col-sm-8">
+			<?php if (have_posts()) : ?>
+			<?php while (have_posts()) : ?>
 			<?php the_post(); ?>
-			<?php if (get_the_post_thumbnail($post, 'full')) : ?>
-				<div class="destaque-thumbnail-pages"><?= get_the_post_thumbnail($post, 'full') ?></div>
+
+			<?php $thumbnail = get_the_post_thumbnail($post, 'full', ['title' => get_the_title(), 'alt' => get_the_title(), 'class' => 'img_single_post']); ?>
+
+			<!-- Verificando se existe uma imagem destacada -->
+			<?php if ($thumbnail) : ?>
+			<?= $thumbnail ?>
 			<?php endif; ?>
-			<div class="description-pages">
-				<?php the_content(); ?>
+
+			<?php the_title('<h1 class="single-title">', '</h1>') ?>
+
+			<?php the_content(); ?>
+
+			<?php endwhile; ?>
+			<?php endif; ?>
+
+			<!-- POSTS RELACIONADOS -->
+			<?php query_posts(array('posts_per_page' => 4, 'orderby' => 'rand')); ?>
+			<?php if (have_posts()) : ?>
+			<hr>
+			<div class="posts-relacionados">
+				<h2>Posts Relacionados</h2>
+				<?php while (have_posts()) : ?>
+				<?php the_post(); ?>
+				<a class="box-list-blog" href="<?= get_permalink() ?>">
+					<?= get_the_post_thumbnail($post->ID, 'loop_blog_thumbnail') ?>
+					<span class="description-post-list">
+						<span class="title"><?= get_the_title() ?></span>
+						<span class="description"><?= get_the_excerpt() ?></span>
+					</span>
+				</a>
+				<br />
+				<?php endwhile; ?>
 			</div>
-		<?php endwhile; ?>
-	<?php endif; ?>
+			<?php endif; ?>
+			<?php wp_reset_query(); ?>
+		</div>
+		<div class="col-sm-4">
+			<aside class="sidebar">
+				<?php if (is_active_sidebar('category-sidebar')) : ?>
+				<?php dynamic_sidebar('category-sidebar') ?>
+				<?php endif; ?>
+			</aside>
+		</div>
+	</div>
 </main>
+
 <?php get_template_part('includes/footer-page'); ?>
